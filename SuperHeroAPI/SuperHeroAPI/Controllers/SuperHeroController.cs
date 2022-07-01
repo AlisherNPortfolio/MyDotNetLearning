2834f536607e3d8c8046eb1d55dbf1e7e4022156
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-07-01 17:52:52 +0500

    init

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SuperHeroController : ControllerBase
+	{
+		private static List<SuperHero> heroes = new List<SuperHero>
+		{
+			new SuperHero { 
+					Id = 1, Name = "Spider Man", 
+					FirstName = "Parker", 
+					LastName = "Parker", 
+					Place = "New York City"
+			},
+			new SuperHero {
+				Id = 2,
+				Name = "string",
+				FirstName = "string",
+				LastName = "string",
+				Place = "string"
+			},
+			new SuperHero {
+				Id = 3,
+				Name = "Ironman",
+				FirstName = "Tony",
+				LastName = "Stark",
+				Place = "Long Island"
+			 }
+		};
+
+		[HttpGet]
+		public async Task<ActionResult<List<SuperHero>>> Get()
+		{
+			return Ok(heroes);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+		{
+			heroes.Add(hero);
+			return Ok(heroes);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<SuperHero>> Get(int id)
+		{
+			var hero = heroes.Find(h => h.Id == id);
+			if (hero == null)
+			{
+				return BadRequest("Hero not found!");
+			}
+
+			return Ok(hero);
+		}
+	}
+}
